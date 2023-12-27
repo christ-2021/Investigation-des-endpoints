f4bc32fabd68c93398d71bf4670fe7a19a7f1ebe
christ-2021 <89962613+christ-2021@users.noreply.github.com>
2023-12-27 05:42:58 -0500

    Add files via upload

---
+++ expand-archive-triage-data-rev3.ps1
@@ -0,0 +1,10 @@
+﻿#Must use PowerShell 7.x to avoid long path/filename output issues
+#Change the three variables below to match your file/folder paths
+$casename = '2023-0829-abc'
+$compressed_triage_data = "D:\cases\$casename\triage_data"
+$unzipped_triage_data = "D:\cases\$casename\triage_data"
+
+(get-childitem -path $compressed_triage_data -filter *.zip).basename | ForEach-Object {
+    
+    Expand-Archive -path $compressed_triage_data\$_.zip -DestinationPath $unzipped_triage_data\$_ -Force
+}
